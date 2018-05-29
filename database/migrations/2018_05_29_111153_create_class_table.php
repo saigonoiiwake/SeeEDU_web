837f47f660bf1f036fa900ed9c46081184a9ff62
tony.jicheng <am362411@gmail.com>
2018-05-29 20:20:48 +0900

    add class table

---
+++ database/migrations/2018_05_29_111153_create_class_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('class', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->int('category_id');
+            $table->timestamps(); // for created_at and updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('class');
+    }
+}
