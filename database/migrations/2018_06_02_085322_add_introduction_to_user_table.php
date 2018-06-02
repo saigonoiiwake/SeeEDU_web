7a3d19b4e40a6c65de6dad1674eee3c56bd3c573
tony.jicheng <am362411@gmail.com>
2018-06-02 18:07:41 +0900

    fix user table

---
+++ database/migrations/2018_06_02_085322_add_introduction_to_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIntroductionToUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->text('intrduction')->after('email')->nullable();
+            $table->json('experience')->after('intrduction')->nullable();
+            $table->json('education')->after('experience')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->dropColumn('intrduction');
+            $table->dropColumn('experience');
+            $table->dropColumn('education');
+        });
+    }
+}
