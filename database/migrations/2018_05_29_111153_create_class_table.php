1685f940307a7c1608246e4f275d0feb170e0cb6
tony.jicheng <am362411@gmail.com>
2018-06-02 14:57:52 +0900

    add class table

--- database/migrations/2018_05_29_111153_create_class_table.php
+++ database/migrations/2018_05_29_111153_create_class_table.php
@@ -14,10 +14,21 @@ class CreateClassTable extends Migration
     public function up()
     {
         Schema::create('class', function (Blueprint $table) {
-            $table->increments('id');
+            $table->unsignedInteger('id')->comment('Class id');
             $table->string('name');
-            $table->int('category_id');
-            $table->timestamps(); // for created_at and updated_at
+            // class status: reviewing, open, rejected...
+            $table->string('status')->default('reviewing')->comment('Status for the class');     
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('min_number');
+            $table->unsignedInteger('max_number');
+            $table->string('currency', 3);
+            $table->float('price');
+            $table->float('early_bird_price')->nullable();
+            $table->unsignedInteger('early_bird_number')->nullable();
+            $table->timestampTz('start_date');
+            $table->timestampTz('end_date');
+            $table->json('data')->nullable()->comment('Metadata');        
+            $table->timestampsTz();      // for created_at and updated_at
         });
     }
 
