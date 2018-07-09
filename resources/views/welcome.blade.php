956a2e1b8d5d099d9a2a3ce953b2c424326ab208
黃惠農 <nung@nous-MacBook-Pro.local>
2018-07-09 14:25:05 +0900

    fix welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    おはよございます！
                 </div>
             </div>
         </div>
