9a1a788af11e9eb69f11dd9e2f80d85b96fb72fa
黃惠農 <huang.h.af@m.titech.ac.jp>
2018-08-06 17:54:56 +0900

    fix/GoogleAnalytics

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115929258-3"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-115929258-3');
+  </script>
+  
   <meta charset="UTF-8">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
