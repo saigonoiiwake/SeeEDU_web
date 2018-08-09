96eea755bfdc61cedb362f58e8b808e10cee7043
黃惠農 <huang.h.af@m.titech.ac.jp>
2018-08-09 18:21:58 +0900

    fix1.3

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Mockery\Exception;
+use Illuminate\Support\Facades\DB;
+use Socialite;
+use App\User;
 
 class LoginController extends Controller
 {
@@ -26,9 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-   protected $redirectTo = '/';
-
-
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -40,108 +41,105 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    public function redirectToProvider_FB()
+   {
+       return Socialite::driver('facebook')->redirect();
+   }
 
-          public function redirectToProvider_FB()
-         {
-             return Socialite::driver('facebook')->redirect();
-         }
+   public function redirectToProvider_G()
+  {
+      return Socialite::driver('google')->redirect();
+  }
 
-         public function redirectToProvider_G()
-        {
-            return Socialite::driver('google')->redirect();
-        }
+   public function handleProviderCallback_FB()
+    {
+        $userSocial = Socialite::driver('facebook')->user();
 
-         public function handleProviderCallback_FB()
+        $token = $userSocial->token;
+        $refreshToken = $userSocial->refreshToken; // not always provided
+        $expiresIn = $userSocial->expiresIn;
+
+        DB::beginTransaction();
+
+        try {
+
+          $data = [
+            'nick_name' => $userSocial->getName(),
+            'email' => $userSocial->getEmail(),
+            'avatar' => $userSocial->getAvatar(),
+            'password' =>  bcrypt(123456),
+          ];
+
+          $findUser = User::where('email', $userSocial->getEmail())->first();
+
+          if( $findUser )
           {
-              $userSocial = Socialite::driver('facebook')->user();
-
-              $token = $userSocial->token;
-              $refreshToken = $userSocial->refreshToken; // not always provided
-              $expiresIn = $userSocial->expiresIn;
-
-              DB::beginTransaction();
-
-              try {
-
-                $data = [
-                  'nick_name' => $userSocial->getName(),
-                  'email' => $userSocial->getEmail(),
-                  'avatar' => $userSocial->getAvatar(),
-                  'password' =>  bcrypt(123456),
-                ];
-
-                $findUser = User::where('email', $userSocial->getEmail())->first();
-
-                if( $findUser )
-                {
-                  \Auth::login($findUser);
-
-                }
-                else {
-                  User::newUser($data);
-
-                  $newUser = User::where('email', $userSocial->getEmail())->first();
-
-                  \Auth::login($newUser);
-
-                  DB::commit();
-                }
-
-                return back();
-
-
-              } catch (\Exception $e) {
-                  DB::rollback();
-                  throw $e;
-              }
+            \Auth::login($findUser);
 
           }
+          else {
+            User::newUser($data);
 
-          public function handleProviderCallback_G()
+            $newUser = User::where('email', $userSocial->getEmail())->first();
+
+            \Auth::login($newUser);
+
+            DB::commit();
+          }
+
+          return redirect('/');
+
+
+        } catch (\Exception $e) {
+            DB::rollback();
+            throw $e;
+        }
+
+    }
+
+    public function handleProviderCallback_G()
+     {
+         $userSocial = Socialite::driver('google')->user();
+
+         $token = $userSocial->token;
+         $refreshToken = $userSocial->refreshToken; // not always provided
+         $expiresIn = $userSocial->expiresIn;
+
+         DB::beginTransaction();
+
+         try {
+
+           $data = [
+             'nick_name' => $userSocial->getName(),
+             'email' => $userSocial->getEmail(),
+             'avatar' => $userSocial->getAvatar(),
+             'password' =>  bcrypt(123456),
+           ];
+
+           $findUser = User::where('email', $userSocial->getEmail())->first();
+
+           if( $findUser )
            {
-               $userSocial = Socialite::driver('google')->user();
-
-               $token = $userSocial->token;
-               $refreshToken = $userSocial->refreshToken; // not always provided
-               $expiresIn = $userSocial->expiresIn;
-
-               DB::beginTransaction();
-
-               try {
-
-                 $data = [
-                   'nick_name' => $userSocial->getName(),
-                   'email' => $userSocial->getEmail(),
-                   'avatar' => $userSocial->getAvatar(),
-                   'password' =>  bcrypt(123456),
-                 ];
-
-                 $findUser = User::where('email', $userSocial->getEmail())->first();
-
-                 if( $findUser )
-                 {
-                   \Auth::login($findUser);
-
-                 }
-                 else {
-                   User::newUser($data);
-
-                   $newUser = User::where('email', $userSocial->getEmail())->first();
-
-                   \Auth::login($newUser);
-
-                   DB::commit();
-                 }
-
-                  return back();
-
-
-               } catch (\Exception $e) {
-                   DB::rollback();
-                   throw $e;
-               }
+             \Auth::login($findUser);
 
            }
+           else {
+             User::newUser($data);
+
+             $newUser = User::where('email', $userSocial->getEmail())->first();
+
+             \Auth::login($newUser);
+
+             DB::commit();
+           }
+
+           return redirect('/');
 
 
+         } catch (\Exception $e) {
+             DB::rollback();
+             throw $e;
+         }
+
+     }
 }
