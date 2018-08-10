cf5ed091d17d23f5cff7704ab4ce0f4e4f5ba6c8
黃惠農 <huang.h.af@m.titech.ac.jp>
2018-08-10 16:58:06 +0900

    fb pixel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Facebook Pixel Code -->
+  <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window,document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '298945697519327');
+      fbq('track', 'PageView');
+  </script>
+  <noscript>
+       <img height="1" width="1"
+      src="https://www.facebook.com/tr?id=298945697519327&ev=PageView
+      &noscript=1"/>
+  </noscript>
+    <!-- End Facebook Pixel Code -->
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
