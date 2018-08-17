e6313e6063e7db6200baf79e16781f253b50b880
黃惠農 <huang.h.af@m.titech.ac.jp>
2018-08-17 23:03:20 +0900

    fix/1.2/redirectToCourses

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/courses';
 
     /**
      * Create a new controller instance.
