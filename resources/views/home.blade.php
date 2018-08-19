2f883215a382eb84c52bf0aac820c9e723051786
黃惠農 <huang.h.af@m.titech.ac.jp>
2018-08-19 23:33:51 +0900

    fixPosition

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -418,9 +418,23 @@
 
    </div>
    <!-- 選課去 -->
-   <a href="{{ route('courses') }}" class="btn blue-bg btn-lg pix-white wide">
-    <span class="pix_edit_text"><b>更多課程</b></span>
-   </a>
+
+   <div class="container">
+    <div class="row">
+     <div class="col-md-12 col-xs-12 col-sm-12 column ui-droppable">
+      <div class="pix-content text-center">
+       <h6 class="pix-black-gray-dark text-center pix-no-margin-top secondary-font">
+
+      </h6>
+       <a href="{{ route('courses') }}" class="btn blue-bg btn-lg pix-white wide">
+        <span class="pix_edit_text"><b>更多課程</b></span>
+       </a>
+      </div>
+     </div>
+    </div>
+   </div>
+   <br>
+
   </div>
  </div>
 
