67b53ba03c7e22dca2c5009960c25381f0b26a8a
John Fu <john80510@gmail.com>
2018-11-16 16:58:50 +0800

    add status column to transaction table

--- app/Transaction.php
+++ app/Transaction.php
@@ -15,7 +15,7 @@ class Transaction extends Model
      * @var array
      */
     protected $fillable = [
-        'user_id', 'course_id', 'purchase_price', 'channel', 'coupon_code'
+        'user_id', 'course_id', 'purchase_price', 'channel', 'coupon_code', 'transaction_status'
     ];
 
     public function user()
