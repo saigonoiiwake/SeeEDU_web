fefec2d450a80e120a009bcc76a3af0c7fe3581d
John Fu <john80510@gmail.com>
2018-11-19 17:59:59 +0800

    [Fix bug] in last commit

--- app/Http/Controllers/SPGController.php
+++ app/Http/Controllers/SPGController.php
@@ -98,7 +98,7 @@ class SPGController extends Controller
   {
     $tradeInfo = MPG::parse(request()->TradeInfo);
     $order_no = $tradeInfo->Result->MerchantOrderNo;
-    $order = Transaction::where('merchant_order_no', $order_no);
+    $order = Transaction::where('merchant_order_no', $order_no)->first();
     if($tradeInfo->Status == 'SUCCESS')
     {  
       $order->transaction_status = 1;
@@ -115,7 +115,18 @@ class SPGController extends Controller
   {
     $info = 'e2faaab427e3857acd5ddab34cbbe0e135998fa17fc66ab2e5b1960e7eb09bfe89d49eb8e2fe92a1b73df25ab3c63ef3c6ee13c9266d650d099b8eb425be6de7cf65869402a54d41822611d7d0da5af2413b02b3a6345c5a2194bdba50da00009b3c36870959fca183fdc8aa48a75b8d23cb59b0019e6439b9c1795ec08839d9be0d7aff9d061b2ccc31be05605b23c93629dce75f776be3db704069a171c2ae32308ef200b71d6b694f10b3ef0c297222a99964c4845baf7099aa0c19ef348b86acb27592cf33ea94961e01507c39202c1078ff80ef357ce532432e43f74f12375c3638b875f2a2bc2ce00e2cfdc93ef131234800b1ff0f805d82049858125ff82c20fa4b43c961818d9e253c3e81a91228b130300289b8ae71a1f29eac88b274136615eeafc874e0cb9bac2009d5aba342101de5966e00162f0051dada37957b31470084efb57b3d022590f2db41d4f48433d2bd34ed90864a839663fa209d83523e610c86dcaea77a058d1283192d484e515d8859f6d6a6ee9bad1e3858988954210505abe16f60d1c5bc4acb833aed2b3fb018d143facd63e29056562fcf70b9230864410d387a3eb2e8731a64a12a21a43fd4f42314c1aa6bbd213b348ac55f9e94444ef3be193274270fd69317bbf604253f5c40f3f6f2b99646b1ccb5';
     $tradeInfo = MPG::parse($info);
-    dd($tradeInfo);
+    $order_no = $tradeInfo->Result->MerchantOrderNo;
+    $order = Transaction::where('merchant_order_no', $order_no)->first();
+    if($tradeInfo->Status == 'SUCCESS')
+    {  
+      $order->transaction_status = 1;
+    }else{
+      $order->transaction_status = $tradeInfo->Status;
+    }
+    // $order = Transaction::orderBy('created_at', 'desc')->first();
+    // $order->info = request()->TradeInfo;
+    $order->save();
+    // dd($tradeInfo);
     // return response()->json($tradeInfo);
     // dd($tradeInfo->Result->MerchantOrderNo);
     // return $tradeInfo;
