88f6d7dc80a0bda3c2214ef78821f52020f4c017
John Fu <john80510@gmail.com>
2018-12-03 16:13:36 +0800

    -test if this solve notify url return error code 302

--- app/Http/Controllers/SPGController.php
+++ app/Http/Controllers/SPGController.php
@@ -23,7 +23,7 @@ class SPGController extends Controller
 
   public function __construct()
   {
-      $this->middleware('auth');
+      $this->middleware('auth', ['except'=>'notify']);
       if (env('APP_ENV') === 'production') {
         $this->serverUrl = 'https://seeedu.org';
       } else {
